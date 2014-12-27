b9080b6f8dea6a90a45878669afcb071a0f39748
xensky <xensky@gmail.com>
2014-12-26 19:14:15 -0500

    okay the mouse grab actually works a bit better now

--- desktop/src-common/butcher/core.clj
+++ desktop/src-common/butcher/core.clj
@@ -7,10 +7,24 @@
 (def manager (asset-manager))
 (set-asset-manager! manager)
 
-(defn rotate
-  [entity x y]
+(defn rotate!
+  [entity x z]
+  (let [trans (-> entity :object (. transform))
+        x? (not (zero? x))
+        z? (not (zero? z))
+        degrees (cond
+                 (and x? z?) (+ x z)
+                 x? x
+                 z? z
+                 :else 0)]
+    ;; looked at the docs, rotate ix x y z axes and degrees
+    ;; set axes to rotate around (0 -> 1?) and then strength as degrees
+    (matrix-4! trans :rotate (if x? 1 0) 0 (if z? 1 0) degrees)))
+
+(defn move-flat
+  [entity x z]
   (let [trans (-> entity :object (. transform))]
-    (matrix-4! trans :rotate 0 x y 1)))
+    (matrix-4! trans :translate x 0 z)))
 
 (defscreen main-screen
   :on-show
@@ -18,7 +32,7 @@
     (update! screen
              :renderer (model-batch)
              :camera (doto (perspective 75 (game :width) (game :height))
-                       (position! 0 5 5)
+                       (position! 0 3 3)
                        (direction! 0 0 0)
                        (near! 0.1)
                        (far! 300)))
@@ -31,21 +45,21 @@
           (assoc :x 0 :y 0 :z 0 :id :box))))
 
   :on-render
-  (fn [screen entities]
+  (fn [{:keys [drag-x drag-y] :as screen} entities]
     (clear! 0 0 0 1)
-    (let [dragging (:dragging screen)
-          drag-x (:drag-x screen)
-          drag-y (:drag-y screen)
-          x (game :x)
-          y (game :y)]
-      (update! screen
-               :drag-x (- x (or drag-x x))
-               :drag-y (- y (or drag-y y)))
+    (let [x (game :x)
+          y (game :y)
+          delta-x (- x (or drag-x x))
+          delta-y (- y (or drag-y y))]
+      (when drag-x
+        (update! screen
+                 :drag-x x
+                 :drag-y y))
       (->> (for [entity entities]
              (case (:id entity)
-               :box (if dragging
+               :box (if drag-x
                       (doto entity
-                        (rotate drag-x drag-y))
+                        (rotate! delta-x delta-y))
                       entity)
                entity))
            (render! screen))))
@@ -55,17 +69,21 @@
 
   :on-touch-down
   (fn [screen entities]
-    (update! screen :dragging true)
+    (update! screen
+             :drag-x (game :x)
+             :drag-y (game :y))
     nil)
 
   :on-touch-up
   (fn [screen entities]
-    (update! screen :dragging false)
+    (update! screen
+             :drag-x nil
+             :drag-y nil)
     nil)
   
   :on-resize
   (fn [screen entities]
-    (height! screen 300)))
+    (height! screen 600)))
 
 (defscreen text-screen
   :on-show
@@ -107,7 +125,7 @@
   
   :on-resize
   (fn [screen entities]
-    (height! screen 300)))
+    (height! screen 600)))
 
 (defgame butcher
   :on-create
