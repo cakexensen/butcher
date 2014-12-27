4bb0ad494257d9cd3144ffc412c6f514dae09a25
xensky <xensky@gmail.com>
2014-12-27 13:43:01 -0500

    move camera as player moves box

--- desktop/src-common/butcher/core.clj
+++ desktop/src-common/butcher/core.clj
@@ -8,35 +8,45 @@
 (def manager (asset-manager))
 (set-asset-manager! manager)
 
+(defn update-screen!
+  [screen entities]
+  ;; move camera with to center on the box
+  (doseq [{:keys [x z id]} entities]
+    (when (= id :box)
+      (position! screen x 5 z)))
+  entities)
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen
              :renderer (model-batch)
              :camera (doto (perspective 75 (game :width) (game :height))
-                       (position! 3 3 -3)
-                       (direction! 0 0 0)
+                       (position! 0 5 0)
+                       (direction! 0 2 0)
                        (near! 0.1)
                        (far! 300)))
-    (assoc (e/box 2 2 2 (color :blue) 0 0 0 :box)
-      :on-render (fn [{:keys [x z] :as this}]
-                   (let [left? (key-pressed? :dpad-left)
-                         right? (key-pressed? :dpad-right)
-                         up? (key-pressed? :dpad-up)
-                         down? (key-pressed? :dpad-down)
-                         x-vel (cond
-                                left? -1
-                                right? 1
-                                :else 0)
-                         z-vel (cond
-                                up? 1
-                                down? -1
-                                :else 0)
-                         x-vel (/ x-vel 20)
-                         z-vel (/ z-vel 20)
-                         x (+ x x-vel)
-                         z (+ z z-vel)]
-                     (assoc this :x x :z z)))))
+    [(assoc (e/box 2 2 2 (color :blue) 0 0 0 :box)
+       :on-render (fn [{:keys [x z] :as this}]
+                    (let [left? (key-pressed? :dpad-left)
+                          right? (key-pressed? :dpad-right)
+                          up? (key-pressed? :dpad-up)
+                          down? (key-pressed? :dpad-down)
+                          x-vel (cond
+                                 left? -1
+                                 right? 1
+                                 :else 0)
+                          z-vel (cond
+                                 up? -1
+                                 down? 1
+                                 :else 0)
+                          x-vel (/ x-vel 20)
+                          z-vel (/ z-vel 20)
+                          x (+ x x-vel)
+                          z (+ z z-vel)]
+                      (assoc this :x x :z z))))
+     (e/box 1 2 3 (color :red) 4 0 0 :b1)
+     (e/box 3 3 2 (color :yellow) -2 0 -4 :b2)])
 
   :on-render
   (fn [screen entities]
@@ -46,7 +56,8 @@
            (if (:on-render entity)
              ((:on-render entity) entity)
              entity))
-         (render! screen)))
+         (render! screen)
+         (update-screen! screen)))
   
   :on-key-down
   (fn [screen entities]
