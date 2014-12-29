a537d6e118082463afb290e1d39ae29b1f8b50a1
xensky <xensky@gmail.com>
2014-12-28 19:06:05 -0500

    add pixelation to render (thanks oakes)

--- desktop/src-common/butcher/core.clj
+++ desktop/src-common/butcher/core.clj
@@ -3,7 +3,10 @@
             [play-clj.g3d :refer :all]
             [play-clj.math :refer :all]
             [play-clj.ui :refer :all]
-            [butcher.entity.core :as e]))
+            [butcher.entity.core :as e])
+  (:import [com.badlogic.gdx.graphics Texture$TextureFilter]
+           [com.badlogic.gdx.graphics.g2d SpriteBatch]
+           [com.badlogic.gdx.graphics.glutils FrameBuffer]))
 
 (def manager (asset-manager))
 (set-asset-manager! manager)
@@ -19,6 +22,8 @@
       (position! screen x camera-y (+ z camera-z))))
   entities)
 
+(def pixelate-factor 4)
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
@@ -28,19 +33,37 @@
                        (position! 0 camera-y camera-z)
                        (direction! 0 2 0)
                        (near! 0.1)
-                       (far! 300)))
+                       (far! 300))
+             :fbo (doto (FrameBuffer. (pixmap-format :r-g-b-888)
+                                      (/ (game :width) pixelate-factor)
+                                      (/ (game :height) pixelate-factor)
+                                      true)
+                    (-> .getColorBufferTexture
+                        (.setFilter
+                         Texture$TextureFilter/Nearest
+                         Texture$TextureFilter/Nearest)))
+             :fbo-batch (SpriteBatch.))
     (conj (e/make-boxes 20) (e/player)))
 
   :on-render
-  (fn [screen entities]
+  (fn [{:keys [fbo fbo-batch] :as screen} entities]
+    (.begin fbo)
     (clear! 0 0 0 1)
-    (->> (for [entity entities]
-           ;; call entity render fns
-           (if (:on-render entity)
-             ((:on-render entity) entity entities)
-             entity))
-         (render! screen)
-         (update-screen! screen)))
+    (let [entities
+          (->> (for [entity entities]
+                 ;; call entity render fns
+                 (if (:on-render entity)
+                   ((:on-render entity) entity entities)
+                   entity))
+               (render! screen)
+               (update-screen! screen))]
+      (.end fbo)
+      (.begin fbo-batch)
+      (.draw fbo-batch
+             (.getColorBufferTexture fbo)
+             0 0 (game :width) (game :height) 0 0 1 1)
+      (.end fbo-batch)
+      entities))
   
   :on-key-down
   (fn [screen entities]
